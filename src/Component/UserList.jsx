3f32520b4ee986209649504514a3098c7348a631
hossem404 <hlagha2020@gmail.com>
2024-04-26 23:10:44 +0100

    first commit

---
+++ src/Component/UserList.jsx
@@ -0,0 +1,45 @@
+import React,{useState,useEffect   } from "react";
+import axios from 'axios';
+
+
+function UserList() {
+  const [listofuser,setlistofuser] = useState([])
+  useEffect(()=>{
+    const fetchData = async () => {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        setlistofuser(response.data);
+
+    } 
+    fetchData();
+})
+  
+  
+    return (
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>username</th>
+                    <th>email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listofuser.map(user => (
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.username}</td>
+                        <td>{user.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+
+
+    </div>
+  )
+}
+
+export default UserList
